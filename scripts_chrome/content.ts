50a04e6f0f0884a65dc32f47ed4403d719438d53
rijalbinhusen <jeryzal333@gmail.com>
2025-08-20 05:50:24 +0700

    fix some conflict

--- scripts_chrome/content.ts
+++ scripts_chrome/content.ts
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'runContentFunction') {
-    alert('Content script function executed!');
-    // reply message to background ts and forward to sidepanel
-    chrome.runtime.sendMessage({ action: 'reply-to-sidepanel', data: 'Hello from content.js!' });
-  }
-});
-=======
 import { type messageCrossScript } from "./scripts_chrome.types";
 
 chrome.runtime.onMessage.addListener( async (message: messageCrossScript, sender, sendResponse) => {
@@ -20,4 +11,3 @@ chrome.runtime.onMessage.addListener( async (message: messageCrossScript, sender
 function sendActionToBackgrouond (data: messageCrossScript) {
   chrome.runtime.sendMessage({ action: data.action, data: data.data });
 }
->>>>>>> feat-google-account-access
