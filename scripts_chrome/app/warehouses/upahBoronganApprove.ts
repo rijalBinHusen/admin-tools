72f6be3127d5e8307a81ffd0e9aec70a053810f6
rijalbinhusen <jeryzal333@gmail.com>
2025-10-08 08:26:03 +0700

    approve upah on the way

--- scripts_chrome/app/warehouses/upahBoronganApprove.ts
+++ scripts_chrome/app/warehouses/upahBoronganApprove.ts
@@ -2,7 +2,7 @@
 
 class UpahBoronganApprove {
   
-    private async loginToApp(name: string, password: string): Promise<boolean> {
+  private async loginToApp(name: string, password: string): Promise<boolean> {
     try {
       const url = '/warehouse/auth/login';
       const body = `username=${name}&password=${password}`;
@@ -14,7 +14,7 @@ class UpahBoronganApprove {
         body,
         referrer: "/warehouse/",
         mode: 'cors',
-        credentials: 'omit',
+        credentials: 'include',
       });
 
       if (!response.ok) {
@@ -33,7 +33,22 @@ class UpahBoronganApprove {
     }
   }
 
-  private async checkUsername(username: string): Promise<string> {
+  private logOut() {
+    return fetch("/warehouse/auth/logout", {
+      "headers": {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-US,en;q=0.9",
+        "upgrade-insecure-requests": "1"
+      },
+      "referrer": "/warehouse/relation/user_gudang",
+      "body": null,
+      "method": "GET",
+      "mode": "cors",
+      "credentials": "include"
+    });
+  }
+
+  private async checkUsername(username: string): Promise<boolean> {
     try {
         const url = '/warehouse/user/profile';
         const response = await fetch(url, { referrer: "/warehouse/" });
@@ -44,17 +59,17 @@ class UpahBoronganApprove {
       
         const responseAsHTMLElemen = new DOMParser().parseFromString(html, "text/html");
         const userElement = responseAsHTMLElemen.querySelector('#nama');
-        if (userElement) {
-            return userElement.textContent;
-        }
-        return "";
+        if (!userElement) throw new Error("Tidak boleh mendapatkan element nama user");
+        
+        const usernameMatched = userElement.textContent == username;
+        return usernameMatched
     } catch (error) {
         console.error('Error checking username:', error);
         throw error;
     }
   }
 
-  private async getListToApprove(date1: string, date2: string): Promise<any[]> {
+  private async getListToApprove(date1: string, date2: string, approvePeople:1|2): Promise<number[]> {
     try {
         const response = await fetch(`/warehouse/generate/get_list_approve?tgl1=${date1}&tgl2=${date2}&get_gd%5B%5D=5&get_gd%5B%5D=4&get_gd%5B%5D=2&get_gd%5B%5D=3&get_gd%5B%5D=7&get_gd%5B%5D=1&get_gd%5B%5D=6`, {
             "headers": {
@@ -71,17 +86,24 @@ class UpahBoronganApprove {
       if (!response.ok) {
         throw new Error(`Failed to fetch list: ${response.status}`);
       }
-      const data = await response.json();
+      const data = await response.json() as GetListToApproveResponse;
 
       if (!data.list || !Array.isArray(data.list)) {
         throw new Error('Invalid response format: "list" array is missing or not an array.');
       }
 
-      const listToApprove: any[] = [];
+      const listToApprove: number[] = [];
       for (const item of data.list) {
-        if (item.generated !== '' && item.approved === '1' && item.approved2 === '0') {
-          listToApprove.push(item);
+        let isNeedToPush = false;
+        if(approvePeople == 1) {
+          isNeedToPush = item.generated !== '' && item.approved === '0' && item.approved2 === '0'
         }
+
+        else if(approvePeople == 2) {
+          isNeedToPush = item.generated !== '' && item.approved === '1' && item.approved2 === '0'
+        }
+
+        if (isNeedToPush) listToApprove.push(Number(item.id_paid));
       }
       return listToApprove;
     } catch (error) {
@@ -90,36 +112,54 @@ class UpahBoronganApprove {
     }
   }
 
-  private async approveRecord() {
-    const doApprove = await fetch("/warehouse/generate/edit_approve", {
-    "headers": {
-        "accept": "application/json, text/javascript, */*; q=0.01",
-        "accept-language": "en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7",
-        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "x-requested-with": "XMLHttpRequest"
-    },
-        "referrer": "/warehouse/generate/approve",
-        "body": "flag_column=2&flag_apv=1&id_paid=3543&link=approve",
-        "method": "POST",
-        "mode": "cors",
-        "credentials": "include"
-    });
+  private async approveRecord(flagColumn: 1|2, idPaid:number) {
+    try {
+      const doApprove = await fetch("/warehouse/generate/edit_approve", {
+      "headers": {
+          "accept": "application/json, text/javascript, */*; q=0.01",
+          "accept-language": "en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7",
+          "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+          "x-requested-with": "XMLHttpRequest"
+      },
+          "referrer": "/warehouse/generate/approve",
+          "body": `flag_column=${flagColumn}&flag_apv=1&id_paid=${idPaid}&link=approve`,
+          "method": "POST",
+          "mode": "cors",
+          "credentials": "include"
+      });
+
+      if (!doApprove.ok) {
+        throw new Error(`Login failed with status: ${doApprove.status}`);
+      }
+      
+    } catch (error) {
+      console.error('Error during approve data:', error);
+      throw error;
+    }
   }
 
   async startApproveUpah(param: parameterToApproveUpah) {
     
     try {
-        
+        let peopleApprove = <2|1> 1;
         for(let user of param.users) {
-            // login
-            const login = await this.loginToApp(user.username, user.password)
-            // check username
-            if(!login) throw new Error("Gagal login ke user: " + user.username)
-            // get list to approve
-            const listToApprove = await this.getListToApprove(param.dateStart, param.dateEnd);
-            // approve
-
-            // check is there any record to approve again
+          if(peopleApprove > 2) return;
+          // logout
+          await this.logOut();
+          // login
+          const login = await this.loginToApp(user.username, user.password)
+          if(!login) throw new Error("Gagal login ke user: " + user.username)
+          // check username
+          const isUsernameMatched = await this.checkUsername(user.username);
+          if(!isUsernameMatched) return;
+          // get list to approve
+          const listToApprove = await this.getListToApprove(param.dateStart, param.dateEnd, peopleApprove);
+          // approve
+          for(let approveId of listToApprove) {
+            await this.approveRecord(peopleApprove, approveId)
+          }
+          // add counter
+          peopleApprove += 1
         }
     } catch (error) {
         
@@ -133,7 +173,44 @@ interface parameterToApproveUpah {
     dateEnd: string
 }
 
-
+interface GetListToApproveResponse {
+    "tgl1": string
+    "tgl2": string
+    "tgl_c": string
+    "gudang": {
+        "id": string
+        "gudang": string
+        "kode": string
+        "locid": string
+        "id_area": string
+        "virtual": string
+        "virtual_t": string
+        "virtual_d": string
+        "area": string
+    },
+    "list": 
+        {
+            "id_paid": string
+            "active": string
+            "trno": string
+            "generated": string
+            "approved": string
+            "approved_on": string
+            "approved_by": string
+            "approved2": string
+            "approved2_on": string
+            "approved2_by": string
+            "id_gd": string
+            "gudang": string
+            "upah": string
+            "tgl_koreksi": null,
+            "tgl_bayar": null,
+            "id_gudang": null,
+            "gudang2": null,
+            "k_upah": null
+        }[]
+    "unset": string
+}
 
 
 // generate this type code for me
